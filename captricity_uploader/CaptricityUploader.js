988679129dcf0a9380aea933d167a23e6206352d
AJ <ajr@captricity.com>
2015-08-18 16:52:20 -0700

    fixed up indentations

--- captricity_uploader/CaptricityUploader.js
+++ captricity_uploader/CaptricityUploader.js
@@ -35,79 +35,79 @@ exports.handler = function (event, context) {
     var batchId = null;
 
     async.waterfall([ // get the Batches with status=setup
-            function getBatches(next) {
+        function getBatches(next) {
+            request({
+                    url: 'https://shreddr.captricity.com/api/v1/batch/?status=setup',
+                    headers: auth_headers,
+                    method: "GET",
+                    json: true
+                },
+                function (error, response, body) {
+                    next(null, body);
+                });
+        },
+        function useExistingOrCreateNewBatch(batches, next) {
+            if (batches.length > 0) { // if there is a setup batch, use that one
+                next(null, batches[0]);
+            } else { // else create a new batch to use
+                var dateAndTime = new Date().toISOString();
                 request({
-                        url: 'https://shreddr.captricity.com/api/v1/batch/?status=setup',
+                        url: 'https://shreddr.captricity.com/api/v1/batch/',
                         headers: auth_headers,
-                        method: "GET",
+                        method: "POST",
+                        form: {name: 'AWSLambdaBatch ' + dateAndTime},
                         json: true
                     },
                     function (error, response, body) {
                         next(null, body);
-                    });
-            },
-            function useExistingOrCreateNewBatch(batches, next) {
-                if (batches.length > 0) { // if there is a setup batch, use that one
-                    next(null, batches[0]);
-                } else { // else create a new batch to use
-                    var dateAndTime = new Date().toISOString();
-                    request({
-                            url: 'https://shreddr.captricity.com/api/v1/batch/',
+                });
+            }
+        },
+        function download(batch, next) {
+            batchId = batch.id;
+            // Download the image from S3
+            s3.getObject({
+                    Bucket: srcBucket,
+                    Key: srcKey
+                },
+                next);
+        },
+        function uploadImageToBatch(response, next) {
+
+            console.log("Uploading image " + srcKey + " to batch " + batchId);
+
+            var url = 'https://shreddr.captricity.com/api/v1/batch/' + batchId + '/batch-file/';
+
+            // write the s3 object into a temp file
+            var file = fs.createWriteStream("/tmp/" + srcKey);
+            file.write(response.Body, function () {
+
+                // when it's done writing, open it back up for upload
+                var readStream = fs.createReadStream("/tmp/" + srcKey);
+                readStream.on('open', function (fd) {
+
+                    // uplaod the image to the BatchFile API
+                    request.post({
+                            url: url,
                             headers: auth_headers,
-                            method: "POST",
-                            form: {name: 'AWSLambdaBatch ' + dateAndTime},
+                            formData: {uploaded_file: readStream},
                             json: true
                         },
                         function (error, response, body) {
-                            next(null, body);
-                        });
-                }
-            },
-            function download(batch, next) {
-                batchId = batch.id;
-                // Download the image from S3
-                s3.getObject({
-                        Bucket: srcBucket,
-                        Key: srcKey
-                    },
-                    next);
-            },
-            function uploadImageToBatch(response, next) {
-
-                console.log("Uploading image " + srcKey + " to batch " + batchId);
-
-                var url = 'https://shreddr.captricity.com/api/v1/batch/' + batchId + '/batch-file/';
-
-                // write the s3 object into a temp file
-                var file = fs.createWriteStream("/tmp/" + srcKey);
-                file.write(response.Body, function () {
-
-                    // when it's done writing, open it back up for upload
-                    var readStream = fs.createReadStream("/tmp/" + srcKey);
-                    readStream.on('open', function (fd) {
-
-                        // uplaod the image to the BatchFile API
-                        request.post({
-                                url: url,
-                                headers: auth_headers,
-                                formData: {uploaded_file: readStream},
-                                json: true
-                            },
-                            function (error, response, body) {
-                                if (error) {
-                                    next(error);
-                                } else {
-                                    if (body.hasOwnProperty('errors')) {
-                                        console.log(body);
-                                        next(body.errors, null)
-                                    }
-                                    next(null, body);
+                            if (error) {
+                                next(error);
+                            } else {
+                                if (body.hasOwnProperty('errors')) {
+                                    console.log(body);
+                                    next(body.errors, null)
                                 }
-                            });
-                    })
-                });
-            }
-        ], function (err, body) {
+                                next(null, body);
+                            }
+                        });
+                })
+            });
+        }],
+        function (err, body) {
             if (err) {
                 console.error(
                     'Unable to upload ' + srcBucket + '/' + srcKey +
